b0c73f2cc3afd50e5d1091f8df12dbacfbc6fd53
Иван <NikolaychukIvan@yandex.ru>
2017-07-30 22:17:38 +0400

    move logic of dto creation from dto

--- src/main/java/com/service/ArticleService.java
+++ src/main/java/com/service/ArticleService.java
@@ -1,16 +1,20 @@
 package com.service;
 
 import com.dto.ArticleDto;
+import com.entity.article.Article;
 import com.entity.article.Language;
 import com.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ArticleService {
+    private static SimpleDateFormat ARTICLE_DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+
     private final ArticleRepository articleRepository;
 
     @Autowired
@@ -21,7 +25,13 @@ public class ArticleService {
     public List<ArticleDto> findByLanguage(Language language) {
         return articleRepository.findByLanguage(language)
                 .stream()
-                .map(ArticleDto::new)
+                .map(this::toDto)
                 .collect(Collectors.toList());
+    }
+
+    private ArticleDto toDto(Article article) {
+        ArticleDto articleDto = new ArticleDto(article);
+        articleDto.creationDate = ARTICLE_DATE_FORMAT.format(article.getArticleInfo().getCreationDate());
+        return articleDto;
     }
 }
