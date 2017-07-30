a4990755351c4f146641aab7cfbbbb3241eae70c
Иван <NikolaychukIvan@yandex.ru>
2017-07-30 22:17:57 +0400

    move logic of dto creation from dto

--- src/main/java/com/dto/ArticleDto.java
+++ src/main/java/com/dto/ArticleDto.java
@@ -3,10 +3,6 @@ package com.dto;
 import com.entity.article.Article;
 import com.entity.article.ArticleInfo;
 
-import java.util.Calendar;
-
-import static java.util.Calendar.*;
-
 public class ArticleDto {
     public String preview;
     public String content;
@@ -23,10 +19,5 @@ public class ArticleDto {
         this.articleId = articleInfo.getId();
         this.likesCounter = articleInfo.getLikesCounter().get();
         this.watchesCounter = articleInfo.getWatchesCounter().get();
-        this.creationDate = constructCreationDate(articleInfo.getCreationDate());
-    }
-
-    private String constructCreationDate(Calendar creationDate) {
-        return creationDate.get(DAY_OF_MONTH) + "." + creationDate.get(MONTH) + "." + creationDate.get(YEAR);
     }
 }
